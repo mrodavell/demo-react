5f03671ec1fcd234268266fe5f1a69ec2c8a4467
mrodavell <jomar.llevado@ustp.edu.ph>
2023-06-07 09:27:06 +0800

    cleaned

--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const TodoList = React.lazy(() => import("../components/pages/TodoList"));
 const Home = React.lazy(() => import("../components/pages/Home"));
 const About = React.lazy(() => import("../components/pages/About"));
 const Profile = React.lazy(() => import("../components/pages/Profile"));
